279b7f60d1caba7919128623bf6a039077c2d3b5
gazi04 <gazmendhalili2016@gmail.com>
2026-05-11 20:19:39 +0200

    :sparkles: Using spatie/eloquent-sortable for drag and drop feature

--- app/Models/World.php
+++ app/Models/World.php
@@ -6,10 +6,19 @@ use Illuminate\Database\Eloquent\Attributes\Table;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\EloquentSortable\Sortable;
+use Spatie\EloquentSortable\SortableTrait;
 
 #[Table('worlds')]
-class World extends Model
+class World extends Model implements Sortable
 {
+    use SortableTrait;
+
+    protected $sortable = [
+        'order_column_name' => 'sort_order',
+        'sort_when_creating' => true,
+    ];
+
     protected $casts = [
         'accent_colors' => 'array',
         'is_published' => 'boolean',
