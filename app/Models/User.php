3c897a5931d4339b9e627f7b223b7cb4634c64aa
gazi04 <gazmendhalili2016@gmail.com>
2026-05-27 09:48:48 +0200

    :bug: Forgot to add the new role and progression columns in #[Fillable([...])]

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'role', 'xp', 'level', 'coins'])]
 #[Hidden(['password', 'two_factor_secret', 'two_factor_recovery_codes', 'remember_token'])]
 class User extends Authenticatable implements FilamentUser
 {
