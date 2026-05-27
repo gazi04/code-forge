27e2a5d40ff1fadf36645e133af7c2494a99469c
gazi04 <gazmendhalili2016@gmail.com>
2026-05-27 09:36:25 +0200

    :sparkles: Added endpoints for the StudentLoginController and added auth middleware

--- routes/web.php
+++ routes/web.php
@@ -3,24 +3,26 @@
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\LessonController;
 use App\Http\Controllers\WorldController;
+use App\Http\Controllers\Auth\StudentLoginController;
 use Illuminate\Support\Facades\Route;
-use Laravel\Fortify\Features;
 
-Route::inertia('/', 'Welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
+Route::get('/', function(){
+    return "hi";
+})->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboard', 'Dashboard')->name('dashboard');
+Route::get('/login', [StudentLoginController::class, 'show'])->name('login');
+Route::post('/login/student', [StudentLoginController::class, 'store'])->name('student.login.submit');
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/worlds', [WorldController::class, 'index'])->name('student.worlds.index');
+    Route::get('/worlds/{world:slug}', [WorldController::class, 'show'])->name('student.worlds.show');
+
+    Route::get('/course/{course:slug}', [CourseController::class, 'show'])->name('student.course.show');
+
+    Route::get('/lessons/{lesson:slug}', [LessonController::class, 'show'])->name('student.lessons.show');
+
+    Route::post('/lessons/{lesson:slug}/submit', [LessonController::class, 'submitClaim'])->name('student.lessons.submit');
 });
-
-// The "World Map" - Entry point for the student
-Route::get('/worlds', [WorldController::class, 'index'])->name('student.worlds.index');
-Route::get('/worlds/{world:slug}', [WorldController::class, 'show'])->name('student.worlds.show');
-
-Route::get('/course/{course:slug}', [CourseController::class, 'show'])->name('student.course.show');
-
-// The "Quest" - Individual Lessons
-Route::get('/lessons/{lesson:slug}', [LessonController::class, 'show'])->name('student.lessons.show');
 
 require __DIR__.'/settings.php';
