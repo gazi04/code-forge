2ff1c40b6cf9ca02e0e5240945a69bf40507363e
gazi04 <gazmendhalili2016@gmail.com>
2026-06-01 22:21:32 +0200

    :bug: Request wasn't imported causing crashes

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -7,6 +7,7 @@ use App\Models\Lesson;
 use App\Models\LessonSubmission;
 use App\Models\User;
 use App\Services\ProgressionService;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
